17bfa5ff413449256b2cd06aec98c98e78f297ce
Florian Dahlmanns <florian.dahlmanns@freenet.de>
2014-10-10 08:07:08 +0200

    Added CheckedExceptionConsumer

---
+++ src/main/java/de/beosign/common/lambda/function/CheckedExceptionConsumer.java
@@ -0,0 +1,19 @@
+package de.beosign.common.lambda.function;
+
+import java.util.function.Consumer;
+
+/**
+ * A {@link Consumer} that may throw a specific exception.
+ * 
+ * @author florian
+ * @param <T> input type
+ * @param <E> exception type that may be thrown during execution
+ */
+public interface CheckedExceptionConsumer<T, E extends Exception> extends CheckedConsumer<T> {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    void acceptWithException(T t) throws E;
+}
